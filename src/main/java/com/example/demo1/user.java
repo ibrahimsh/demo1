85f246a1347cd4c88372182cca0c56ba095cd19b
L.B.Asoftware <enge.sibrahim@gmail.com>
2023-11-19 17:45:42 +0200

    adding user class

---
+++ src/main/java/com/example/demo1/user.java
@@ -0,0 +1,16 @@
+package com.example.demo1;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@ToString
+public class user {
+    private  int id  ;
+    private  String  name ;
+
+}
