f3377930d16e9d4c9be1261d0dcbc42a287b2ff9
L.B.Asoftware <enge.sibrahim@gmail.com>
2023-11-19 17:28:57 +0200

    my first springboot app

---
+++ src/main/java/com/example/demo1/control.java
@@ -0,0 +1,15 @@
+package com.example.demo1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Controller
+public class control {
+    @RequestMapping(value = "/welcome")
+    public String start()
+    {
+        return "<h1> this  is  my  first web apps</h1>";
+    }
+}
